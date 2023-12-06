8cb7f2ff707fb3b9026cb3a666f18e35b1700679
moshe vilner <shootermv@gmail.com>
2023-12-06 09:12:33 +0200

    chore: restore strictmode tag

--- src/index.tsx
+++ src/index.tsx
@@ -8,4 +8,8 @@ import './locale/allTranslations'
 ReactGA.initialize('G-0YRQT80GG1')
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
